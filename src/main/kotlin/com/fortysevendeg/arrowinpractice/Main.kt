f166611babc944bdf9938f5cd7ebd892ed67b540
Jorge Castillo <jorge.castillo.prz@gmail.com>
2018-08-14 16:03:27 +0200

    remove Arrow deps for now, add jackson serialization

--- src/main/kotlin/com/fortysevendeg/arrowinpractice/Main.kt
+++ src/main/kotlin/com/fortysevendeg/arrowinpractice/Main.kt
@@ -1,7 +1,11 @@
 package com.fortysevendeg.arrowinpractice
 
 import io.ktor.application.call
+import io.ktor.application.install
+import io.ktor.features.ContentNegotiation
 import io.ktor.http.ContentType
+import io.ktor.jackson.jackson
+import io.ktor.response.respond
 import io.ktor.response.respondText
 import io.ktor.routing.get
 import io.ktor.routing.routing
@@ -10,9 +14,16 @@ import io.ktor.server.netty.Netty
 
 fun main(args: Array<String>) {
   embeddedServer(Netty, 8080) {
+    install(ContentNegotiation) {
+      jackson {}
+    }
+
     routing {
       get("/") {
-        call.respondText("Hello, world!", ContentType.Text.Html)
+        call.respondText("Welcome to the Game of Thrones API!", ContentType.Text.Html)
+      }
+      get("/houses") {
+        call.respond(mapOf("OK" to true))
       }
     }
   }.start(wait = true)
