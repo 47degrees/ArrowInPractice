b54eaf37c5f80f30e44c2e65949836fe1da9a08d
Jorge Castillo <jorge.castillo.prz@gmail.com>
2018-10-09 19:23:11 +0200

    make a test work

--- src/test/kotlin/com/fortysevendeg/arrowinpractice/HousesOverviewTest.kt
+++ src/test/kotlin/com/fortysevendeg/arrowinpractice/HousesOverviewTest.kt
@@ -1,37 +1,20 @@
 package com.fortysevendeg.arrowinpractice
 
-import io.ktor.http.ContentType
-import io.ktor.http.HttpHeaders
+import io.ktor.application.Application
 import io.ktor.http.HttpMethod
 import io.ktor.http.HttpStatusCode
 import io.ktor.server.testing.handleRequest
 import io.ktor.server.testing.withTestApplication
 import org.junit.Test
 import kotlin.test.assertEquals
-import kotlin.test.assertTrue
 
 class HousesOverviewTest {
 
   @Test
-  fun `GET houses overview returns expected serialized housesasdasd`() = withTestApplication {
-    val call = handleRequest(HttpMethod.Get, "/") {
-      addHeader(HttpHeaders.ContentType, ContentType.Application.Json.toString())
-    }
-    with(call) {
+  fun testRequests() = withTestApplication(Application::setupModule) {
+    with(handleRequest(HttpMethod.Get, "/")) {
       assertEquals(HttpStatusCode.OK, response.status())
-    }
-  }
-
-  @Test
-  fun `GET houses overview returns expected serialized houses`(): Unit {
-    withTestApplication(
-      moduleFunction = {
-        main(arrayOf())
-      }
-    ) {
-      handleRequest(HttpMethod.Get, "/").apply {
-        assertTrue { response.content!!.contains("You need to upload some videos to watch them") }
-      }
+      assertEquals("Hello from Ktor Testable sample application", response.content)
     }
   }
 }
